f90fd7a552b9857ee0c02d1262e37474440f7f3b
DStark <hindash@email.arizona.edu>
2020-02-16 16:19:31 -0700

    connection files updates

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,13 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "password",
+  user: process.env.user,
+  password: process.env.db,
   database: "burgers_db"
 });
 
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
