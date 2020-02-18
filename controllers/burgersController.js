edb19c3aeb04288ab83a2b35ad3bafd69791c81a
DStark <hindash@email.arizona.edu>
2020-02-18 12:35:46 -0700

    update to controllers file

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,25 +1,24 @@
 var express = require("express");
-
+require('dotenv').config();
 var router = express.Router();
 var burger = require("../models/burger.js");
 
 // get route -> index
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
+router.get("/burgers", function (req, res) {
+  burger.all(function (burgerData) {
     // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
     res.render("index", { burger_data: burgerData });
   });
 });
 
 // post route -> back to index
-router.post("/burgers/create", function(req, res) {
+router.post("/burgers/create", function (req, res) {
   // takes the request object using it as input for burger.addBurger
-  burger.create(req.body.burger_name, function(result) {
+  burger.create(req.body.burger_name, function (result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
     // render back to index with handle
     console.log(result);
@@ -28,12 +27,11 @@ router.post("/burgers/create", function(req, res) {
 });
 
 // put route -> back to index
-router.put("/burgers/:id", function(req, res) {
-  burger.update(req.params.id, function(result) {
+router.put("/burgers/:id", function (req, res) {
+  burger.update(req.params.id, function (result) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
     console.log(result);
-    // Send back response and let page reload from .then in Ajax
     res.sendStatus(200);
   });
 });
